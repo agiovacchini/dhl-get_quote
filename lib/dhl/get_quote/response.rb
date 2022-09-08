6cda3d9b005158b3a96ed76ac400ad64d73f344d
Andrea Giovacchini <giovacchinia@gmail.com>
2022-09-09 00:49:11 +0200

    Fix

--- lib/dhl/get_quote/response.rb
+++ lib/dhl/get_quote/response.rb
@@ -94,8 +94,8 @@ protected
 
   def condition_indicates_error?
     @parsed_xml["DCTResponse"]["GetQuoteResponse"] &&
-      @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"] &&
-        @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]["Condition"] &&
+      @parsed_xml["DCTResponse"]["GetQuoteResponse"].keys.include?('Note') &&
+        @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"].keys.include?('Condition') &&
           @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]["Condition"].is_a?(Hash)
   end
 
